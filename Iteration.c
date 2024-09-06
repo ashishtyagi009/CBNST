6ab42ffdc4747089b25d1803e8f61bb8bf26482d
Ashish Tyagi <135831029+ashishtyagi009@users.noreply.github.com>
2024-09-06 11:39:25 +0530

    teration.c

--- Iteration.c
+++ Iteration.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h> // For exit function
 
-// Define the function and its derivatives
+// Define the function f(x) = cos(x) - 3*x + 1
 float f(float x) {
     return cos(x) - 3*x + 1;
 }
 
-float fl(float x) {
+// Define the fixed-point iteration functions
+float g1(float x) {
     return (cos(x) + 1) / 3;
 }
 
-float f2(float x) {
+float g2(float x) {
     return acos(3*x - 1);
 }
 
-float dl(float x) {
+// Derivative of g1(x)
+float dg1(float x) {
     return -sin(x) / 3;
 }
 
-float d2(float x) {
+// Derivative of g2(x)
+float dg2(float x) {
     return -3 / sqrt(1 - pow(3*x - 1, 2));
 }
 
@@ -27,17 +29,16 @@ int main() {
     float a, b, x0, h, e;
     int it = 0;
 
-    // User information
-    printf("Name: Ashish Tyagi\nRoll no.: 24\n");
+    // Updated user information
+    printf("Name: Ashish Tyagi\nRoll no.: 24\nSect: D\n");
 
-    // Input guesses for initial values
-    printf("Guess the initial values of a and b for the equation cos(x) = 3x - 1\n"); 
+    // Input initial guesses
+    printf("Guess the initial values of a and b for the equation cos(x) = 3x - 1\n");
     scanf("%f%f", &a, &b);
 
     // Check if initial guesses are valid
     while (f(a) * f(b) > 0) {
-        printf("Invalid guesses! The function values at a and b must have opposite signs.\n");
-        printf("Guess the values again!\n");
+        printf("Invalid guesses!\nGuess the values again!\n");
         scanf("%f%f", &a, &b);
     }
 
@@ -45,36 +46,44 @@ int main() {
     printf("Enter the value of tolerable error\n");
     scanf("%f", &e);
 
-    // Choose initial guess based on function values
+    // Choose the initial point x0
     x0 = (fabs(f(a)) < fabs(f(b))) ? a : b;
 
-    // Output header
-    printf("Iteration\tRoot\n");
+    // Print the derivatives for both methods at the initial guess
+    float dg1_value = dg1(x0);
+    float dg2_value = dg2(x0);
 
-    // Determine which method to use
-    if (fabs(dl(x0)) < 1) {
-        // Using fixed-point iteration with fl
+    printf("valuen of g1'(x0) = %f is %f\n", x0, dg1_value);
+    printf("value of g2'(x0) = %f is %f\n", x0, dg2_value);
+
+    // Choose the iteration method based on the derivative's condition
+    if (fabs(dg1_value) < 1) {
+        // Print the chosen method
+        printf("Chosen method: g1(x)\n");
+        printf("Iteration\tx0\n");
+
         do {
-            h = fl(x0);
+            h = g1(x0);
             x0 = h;
             it++;
             printf("%d\t\t%f\n", it, h);
         } while (fabs(f(h)) > e);
-    } else if (fabs(d2(x0)) < 1) {
-        // Using fixed-point iteration with f2
+    } else if (fabs(dg2_value) < 1) {
+        // Print the chosen method
+        printf("Chosen method: g2(x)\n");
+        printf("Iteration\tx0\n");
+
         do {
-            h = f2(x0);
+            h = g2(x0);
             x0 = h;
             it++;
             printf("%d\t\t%f\n", it, h);
         } while (fabs(f(h)) > e);
     } else {
         printf("Real roots cannot be found in this range\n");
-        exit(0);
+        return 0;
     }
 
-    // Output the result
     printf("The real approximate root after %d iterations is %f\n", it, h);
-
     return 0;
 }
